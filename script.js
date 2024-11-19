6c2df3560cd241012956c68266a8ccd7d9bc8604
MD Affan Asghar <113276865+affancoder@users.noreply.github.com>
2024-11-19 16:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// // script.js
+// document.addEventListener("DOMContentLoaded", () => {
+//     gsap.from(".navbar", { y: -100, duration: 1, opacity: 0 });
+  
+//     gsap.from(".hero-content h1", { y: 50, duration: 1.5, opacity: 0 });
+//     gsap.from(".hero-content p", { x: -100, duration: 1.5, opacity: 0, delay: 0.5 });
+//     gsap.from(".cta-btn", { scale: 0.5, duration: 1.5, opacity: 0, delay: 1 });
+  
+//     gsap.from(".card", { y: 50, duration: 1, opacity: 0, stagger: 0.2 });
+//     gsap.from(".gallery img", { scale: 0.5, duration: 1, opacity: 0, stagger: 0.3 });
+//   });
+  
